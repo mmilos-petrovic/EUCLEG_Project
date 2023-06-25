7b094e03e12ee1bdd25659b1f3da6425afc846d7
Milos Petrovic <petrovicmilos@gmail.com>
2023-06-26 00:52:26 +0200

    first

---
+++ code/main.r
@@ -0,0 +1,13 @@
+
+
+
+source("analize/mape.r")
+source("analize/tabele.r")
+source("analize/grafici.r")
+source("analize/mixedBotanicalTypes.r")
+source("analize/korelacije.r")
+source("analize/cluster.r")
+source("analize/wwwGrafik.r")
+source("analize/ggeStability.r")
+source("analize/pca.r")
+
